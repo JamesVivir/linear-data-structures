2e7d50529407d6991d35b21f0de88130d75ab19c
James Vivir <lovekdbf@gmail.com>
2025-03-05 18:39:18 +0500

    code for start

--- src/algorithms/shuntingYard/index.ts
+++
@@ -1,9 +0,0 @@
-import {ShuntingYard} from "./ShuntingYard";
-
-const shuntingYard = new ShuntingYard();
-
-const rpn = shuntingYard.infixToRPN("815-(4*6)/2-1")
-console.log(rpn)
-
-const result = shuntingYard.evaluateRPN(rpn);
-console.log(result)
--- src/linkedList/doublyLinkedList/index.ts
+++
@@ -1,9 +0,0 @@
-import {DoublyLinkedList, IElement} from "./DoublyLinkedList";
-
-const doublyLinkedList = new DoublyLinkedList();
-
-const n1 = doublyLinkedList.addLast(10);
-const n2 = doublyLinkedList.addLast(20);
-const n3 = doublyLinkedList.addLast(30);
-
-console.log("")
